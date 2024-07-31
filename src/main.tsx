53de390b5301e69d6663828d8cf8fbba62c748a1
IgorStrez <w5813336@gmail.com>
2024-07-31 21:47:37 +0200

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
-import Header from './components/Header';
+import Header from './components';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
